33caffbd927f94483487e1559c01265c58ec90b0
crimrolc9 <crimrolc9@gmail.com>
2018-02-28 21:36:24 +0900

    test

---
+++ script.js
@@ -0,0 +1,9 @@
+for(var a = 1; a <= 5; a++){
+    for(var b = 1; b <= a; b++){
+        if(b == a){
+            document.write("☆");}
+        else{
+            document.write("★");}
+    }
+    document.write("<br>");
+}
